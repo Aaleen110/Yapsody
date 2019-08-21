86c96ba932c3d6fa34fc83d8d3f1417b6156dbdb
Aaleen <aaleenmirza110@gmail.com>
2019-08-21 09:10:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+  },
+});
+
+export default App;
